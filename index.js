ed29781dfae1a524014e2339ec7df4f490959ba9
sakura315 <yuting20010315@gmail.com>
2022-04-18 17:52:23 +0800

    github page

---
+++ index.js
@@ -0,0 +1,18 @@
+function viewInVR(role) {
+
+    switch (role) {
+        case 'spongebob':
+            break;
+            case 'squidward':
+
+            break;
+            case 'patrickstar':
+            break;
+            case 'krabby':
+            break;
+            case 'krusty':
+            break;
+            case 'home':
+            break;
+    }
+}
